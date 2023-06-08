d06d0abee82c27d8497304f4618ea1a278955d3c
Austin Coleman <austinmichaelcoleman@gmail.com>
2023-06-07 20:35:53 -0700

    Add script to change checkout button style

---
+++ change_checkout_button_style.js
@@ -0,0 +1,12 @@
+var checkoutButton = document.getElementById("checkoutButton");
+
+checkoutButton.style.backgroundColor = "green";
+checkoutButton.style.color = "white";
+checkoutButton.style.border = "none";
+checkoutButton.style.padding = "15px 32px";
+checkoutButton.style.textAlign = "center";
+checkoutButton.style.textDecoration = "none";
+checkoutButton.style.display = "inline-block";
+checkoutButton.style.fontSize = "16px";
+checkoutButton.style.margin = "4px 2px";
+checkoutButton.style.cursor = "pointer";
